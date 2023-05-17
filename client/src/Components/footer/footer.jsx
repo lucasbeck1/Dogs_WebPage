bcf84a6a2d059074357c42a27e137f5fa79fcf54
lucasbeck1 <lucasbeckford1@gmail.com>
2023-05-17 03:15:32 -0300

    Update_38

--- client/src/Components/footer/footer.jsx
+++ client/src/Components/footer/footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Vectors from '../../Assets/vectors.js'
 import s from "./footer.module.css";
 
@@ -6,9 +6,37 @@ import s from "./footer.module.css";
 export default function Footer () {
 
   const year = new Date().getFullYear().toString();
+  const [smallScreen, setSmallScreen] = useState(false);
+  const [start, setStart] = useState(true);
+
+  if(window.screen.availWidth <= 590 && smallScreen !== true && start === true){
+    setSmallScreen(true);
+    setStart(false);
+  };
   
   return(
     <React.Fragment>
+       {smallScreen ? (
+        <div className={s.footerSmall}>
+         <div>
+           <span className={s.text}>Made by </span>
+           <span className={s.textBold}>Lucas Beckford</span>
+         </div>
+ 
+         <div className={s.links}>
+          <a href='https://lucasbeck1.github.io/Portfolio/'target='blank'>
+           {Vectors.person}   
+          </a>
+          <a href='https://www.linkedin.com/in/lucas-jbec/'target='blank'>
+           {Vectors.linkedIn}   
+          </a>
+          <a href='https://github.com/lucasbeck1' target='blank'>
+           {Vectors.git}
+          </a>
+         </div>
+         <p className={s.text}>{year} - All rights reserved</p>
+       </div>
+      ):(
       <div className={s.footer}>
         <div>
           <span className={s.text}>Made by </span>
@@ -17,17 +45,18 @@ export default function Footer () {
 
         <div className={s.links}>
           <a href='https://lucasbeck1.github.io/Portfolio/'target='blank'>
-          {Vectors.person}   
+           {Vectors.person}   
           </a>
           <a href='https://www.linkedin.com/in/lucas-jbec/'target='blank'>
-          {Vectors.linkedIn}   
+           {Vectors.linkedIn}   
           </a>
           <a href='https://github.com/lucasbeck1' target='blank'>
-          {Vectors.git}
+           {Vectors.git}
           </a>
         </div>
         <p className={s.text}>{year} - All rights reserved</p>
       </div>
+      )}
     </React.Fragment>
   )
 };
--- client/src/Components/header/header.jsx
+++ client/src/Components/header/header.jsx
@@ -12,6 +12,8 @@ export default function Header ({currentPage}) {
     const allbreeds = useSelector(state => state.allbreeds);
 
     const [name, setName] = useState('');
+    const [smallScreen, setSmallScreen] = useState(false);
+    const [start, setStart] = useState(true);
 
     async function search(e){
         e.preventDefault();
@@ -27,18 +29,46 @@ export default function Header ({currentPage}) {
     };
 
 
+
+    if(window.screen.availWidth <= 590 && smallScreen !== true && start === true){
+        setSmallScreen(true);
+        setStart(false);
+    };
+
+
     return(
         <React.Fragment>
+            {smallScreen ? (
+            <div className={s.headerSmall}>
+                 <h2 className={s.titleSmall}>Dogui Pedia</h2>
+                 <div>
+                    <Link to='/'><button className={s.btn}>To Landing</button></Link>
+                    <Link to='/create'>
+                        <button className={s.btn}>
+                            New {Vectors.dog}
+                            {Vectors.add}
+                        </button>
+                    </Link>
+               
+                    <form>
+                        <input className={s.input} id='SearchInput' type='text' onChange={e => setName(e.target.value)} placeholder="Search..."/>
+                        <button  onClick={e => search(e)} type="submit" className={s.btn}>
+                        {Vectors.search}
+                        </button>
+                    </form>
+                </div>  
+            </div>
+            ) : (
             <div className={s.header}>
                 <div>
                     <form>
-                        <input id='SearchInput' type='text' onChange={e => setName(e.target.value)} placeholder="Search..."/>
+                        <input className={s.input} id='SearchInput' type='text' onChange={e => setName(e.target.value)} placeholder="Search..."/>
                         <button  onClick={e => search(e)} type="submit" className={s.btn}>
                         {Vectors.search}
                         </button>
                     </form>
                 </div>
-                <h2>Dogui Pedia</h2>
+                <h2 className={s.title}>Dogui Pedia</h2>
                 <div>
                     <Link to='/'><button className={s.btn}>To Landing</button></Link>
                     <Link to='/create'>
@@ -49,6 +79,8 @@ export default function Header ({currentPage}) {
                     </Link>
                 </div>
             </div>
+            )}
+            
         </React.Fragment>
     )
 };
