ffd4e58b7e5803db2f17a715114f560bff3f22f0
lucasbeck1 <lucasbeckford1@gmail.com>
2023-05-28 05:10:28 -0300

    Update_43

--- client/src/Components/landing/landing.jsx
+++ client/src/Components/landing/landing.jsx
@@ -8,32 +8,32 @@ import image2 from "../../Assets/back-2.jpg";
 import image3 from "../../Assets/back-3.jpg";
 
 
-
 export default function Landing(){
 
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        dispatch(getBreeds());
-        dispatch(getTemperaments());
-    },[dispatch]);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getBreeds());
+    dispatch(getTemperaments());
+  },[dispatch]);
 
-
-    return(
-        <React.Fragment>
-            <div className={s.pageContainer}>
-                <div className={s.textContainer}>
-                    <h2>Dogui Pedia</h2>
-                    <h4>The Dog Breeds Encyclopedia</h4>
-                    <Link to='/home' >
-                        <button className={s.btnStart}>Get Started</button>
-                    </Link>
-                </div>
-                <div className={s.imageContainer}>
-                    <img src={image1} alt="dogui" className={s.image}/>
-                    <img src={image2} alt="dogui" className={s.image}/>
-                    <img src={image3} alt="dogui" className={s.image}/>
-                </div>
-            </div>
-        </React.Fragment>
-    )
+  return(
+    <React.Fragment>
+      <div className={s.pageContainer}>
+        <div className={s.contentConatiner}>
+          <div className={s.textContainer}>
+            <h2>Dogui Pedia</h2>
+            <h4>The Dog Breeds Encyclopedia</h4>
+            <Link to='/home' >
+              <button className={s.btnStart}>Get Started</button>
+            </Link>
+          </div>
+          <div className={s.imageContainer}>
+            <img src={image1} alt="dogui" className={s.image}/>
+            <img src={image2} alt="dogui" className={s.image}/>
+            <img src={image3} alt="dogui" className={s.image}/>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 };
