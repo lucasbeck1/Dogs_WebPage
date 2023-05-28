7823297420861e245947f66446b6adf8aa500eb0
lucasbeck1 <lucasbeckford1@gmail.com>
2023-05-28 06:18:49 -0300

    Update_45

--- client/src/Components/home/home.jsx
+++ client/src/Components/home/home.jsx
@@ -12,63 +12,61 @@ import s from "./home.module.css";
 
 
 export default function Home(){
-    // Global states
-    let breeds = useSelector(state => state.breeds);
-    const allbreeds = useSelector(state => state.allbreeds);
-    const temperaments = useSelector(state => state.temperaments);
-    
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        if(!allbreeds.length) dispatch(getBreeds());
-        if(!temperaments.length) dispatch(getTemperaments());
-    },[dispatch, allbreeds, temperaments]);
+  // Global states
+  let breeds = useSelector(state => state.breeds);
+  const allbreeds = useSelector(state => state.allbreeds);
+  const temperaments = useSelector(state => state.temperaments);
+  
+  const dispatch = useDispatch();
+  useEffect(()=>{
+      if(!allbreeds.length) dispatch(getBreeds());
+      if(!temperaments.length) dispatch(getTemperaments());
+  },[dispatch, allbreeds, temperaments]);
 
 
-    // Local states
-    const [currentPage, setCurrentPage] = useState(1);
-    const changePage = (pageNumber) => {setCurrentPage(pageNumber)};
-    const previousPage = () => {if(parseInt(currentPage) !== 1) setCurrentPage(parseInt(currentPage) -1)};
-    const nextPage = (last) => {if(parseInt(currentPage) !== (last)) setCurrentPage(parseInt(currentPage) + 1)};
+  // Local states
+  const [currentPage, setCurrentPage] = useState(1);
+  const changePage = (pageNumber) => {setCurrentPage(pageNumber)};
+  const previousPage = () => {if(parseInt(currentPage) !== 1) setCurrentPage(parseInt(currentPage) -1)};
+  const nextPage = (last) => {if(parseInt(currentPage) !== (last)) setCurrentPage(parseInt(currentPage) + 1)};
 
-    //const [cardsPerPage, setcardsPerPage] = useState(8);
-    const cardsPerPage = 8;
-    const indexLastCard = currentPage * cardsPerPage;
-    const ixdexFirstCard = indexLastCard - cardsPerPage;
-    const currentCards = breeds.slice(ixdexFirstCard, indexLastCard);
-    
- 
-    return(
-        <React.Fragment>
-            <div className={s.page}>
-                <Header currentPage={setCurrentPage}/>
-                <>
-                    {allbreeds.length? (
-                        <div className={s.contentContainer}>
-                            <div className={s.content}> 
-                                <SideBar CurrentPage={setCurrentPage}/>
-                                <div className={s.list}>
-                                    {currentCards.map(b=>{return(
-                                        <Card key={b.id} id={b.id} name={b.name} img={b.image} weight={b.weight} temp={b.temperament}/>
-                                    )})}
-                                </div>
-                            </div>
-                            <div>
-                                <Paginated 
-                                dogsTotal={breeds.length} 
-                                dogsPage={cardsPerPage} 
-                                actualPage={parseInt(currentPage)}
-                                select={changePage} 
-                                prevSelect={previousPage}
-                                nextSelect={nextPage} 
-                                />
-                            </div>
-                        </div>  
-                        ) : 
-                        (<Loading loadImg={loadImage} smallImage={true}/>)
-                    }
-                </>
-                <Footer/>
-            </div>
-        </React.Fragment>
-    )
+  //const [cardsPerPage, setcardsPerPage] = useState(8);
+  const cardsPerPage = 8;
+  const indexLastCard = currentPage * cardsPerPage;
+  const ixdexFirstCard = indexLastCard - cardsPerPage;
+  const currentCards = breeds.slice(ixdexFirstCard, indexLastCard);
+  
+
+  return(
+    <React.Fragment>
+      <div className={s.page}>
+        <Header currentPage={setCurrentPage}/>
+        <>
+          {allbreeds.length? (
+            <div className={s.contentContainer}>
+              <Paginated 
+              dogsTotal={breeds.length} 
+              dogsPage={cardsPerPage} 
+              actualPage={parseInt(currentPage)}
+              select={changePage} 
+              prevSelect={previousPage}
+              nextSelect={nextPage} 
+              />
+              <div className={s.content}> 
+                <SideBar CurrentPage={setCurrentPage}/>
+                <div className={s.list}>
+                  {currentCards.map(b=>{return(
+                    <Card key={b.id} id={b.id} name={b.name} img={b.image} weight={b.weight} temp={b.temperament}/>
+                  )})}
+                </div>
+              </div> 
+            </div>  
+            ) : 
+            (<Loading loadImg={loadImage} smallImage={true}/>)
+          }
+        </>
+        <Footer/>
+      </div>
+    </React.Fragment>
+  )
 };
--- client/src/Components/paginated/paginated.jsx
+++ client/src/Components/paginated/paginated.jsx
@@ -3,27 +3,26 @@ import s from "./paginated.module.css";
 
 export default function Paginated({dogsTotal, dogsPage, select, nextSelect, prevSelect, actualPage}){
 
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(dogsTotal/dogsPage); i++) {
-        pageNumbers.push(i)
-    };
-    
-    let firstInPage = actualPage * 8 - 7;
-    let lastInPage = actualPage * 8;
-    if(lastInPage > dogsTotal) lastInPage = dogsTotal;
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(dogsTotal/dogsPage); i++) {
+      pageNumbers.push(i)
+  };
+  
+  let firstInPage = actualPage * 8 - 7;
+  let lastInPage = actualPage * 8;
+  if(lastInPage > dogsTotal) lastInPage = dogsTotal;
 
-    
-    return(
-        <React.Fragment>
-            <p className={s.text}>{firstInPage} - {lastInPage} of {dogsTotal}</p>
-            <nav className={s.nav}>
-                <button onClick={()=>prevSelect()} className={s.number} title="previous">{'<'}</button>
-                {pageNumbers?.map( number => { return(
-                    <button onClick={()=>select(number)} key={number} className={number === actualPage? (s.numberSelected) : (s.number)}>{number}</button>
-                )})}
-                <button onClick={()=>nextSelect(pageNumbers[pageNumbers.length-1])} className={s.number} title="next">{'>'}</button>
-            </nav>
-           
-        </React.Fragment>
-    )
+  
+  return(
+    <React.Fragment>
+      <nav className={s.nav}>
+        <button onClick={()=>prevSelect()} className={s.number} title="previous">{'<'}</button>
+        {pageNumbers?.map( number => { return(
+          <button onClick={()=>select(number)} key={number} className={number === actualPage? (s.numberSelected) : (s.number)}>{number}</button>
+        )})}
+        <button onClick={()=>nextSelect(pageNumbers[pageNumbers.length-1])} className={s.number} title="next">{'>'}</button>
+        <p className={s.text}>{firstInPage} - {lastInPage} of {dogsTotal}</p>
+      </nav>
+    </React.Fragment>
+  )
 };
